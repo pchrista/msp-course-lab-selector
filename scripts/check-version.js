abc3dec78f424b2164b7d68f0a6529dd71d7c254
Arthur Foulon <arthurgallaxy@gmail.com>
2026-06-01 13:58:55 +0200

    fixed file path and testing

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -32,8 +32,8 @@ function getJsonVersion(source) {
     }
 }
 
-const appPath = 'planner-v4.html';
-const versionPath = 'version.json';
+const appPath = 'MSP Course Planner V3/planner-v4.html';
+const versionPath = 'MSP Course Planner V3/version.json';
 
 const beforeApp = readFileAt(baseSha, appPath);
 const afterApp = readFileAt(headSha, appPath);
